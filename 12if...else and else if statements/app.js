9cbd6687a981d1b75c433131be6b30af0a59ad03
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-03 03:50:16 +0500

    chatgpt

---
+++ 12if...else and else if statements/app.js
@@ -0,0 +1,23 @@
+// // In JavaScript, the if...else and else if statements are used for conditional logic, which allows the program to make decisions based on the conditions that are present at runtime.
+
+// // The if...else statement evaluates a condition, and if the condition is true, the code inside the if block is executed. If the condition is false, the code inside the else block is executed. Here is an example:
+// let x=4;
+// if (x>5) {console.log("x isgreater thaen 5")
+    
+// }else{
+//     console.log("x is less then or equal")
+// }
+// //In this example, the condition x > 5 is true, so the code inside the if block is executed, and the output will be "x is greater than 5".
+
+//The else if statement is used to add additional conditions to the if...else statement. This allows the program to check for multiple conditions and execute different code blocks based on the conditions that are met. Here is an example:
+// let x=10;
+// if (x>10) {console.log("x is grater then 10");
+    
+// }else if (x<10) {console.log("x is less then 10");
+    
+// }else{
+//     console.log("x is equal to 10");
+// }
+// //In this example, the first condition x > 10 is false, so the program checks the next condition x < 10. This condition is also false, so the program executes the code inside the else block, and the output will be "x is equal to 10".
+
+//By using if...else and else if statements, you can create complex decision-making structures in your JavaScript code.
---
+++ 13. Testing sets of conditions/app.js
@@ -0,0 +1,7 @@
+//In JavaScript, you can test sets of conditions using logical operators such as && (and), || (or), and ! (not).
+//The && operator tests if all conditions are true. For example:
+let x=65;
+let y=10;
+if (x>0&&y>0) {console.log("both x and y are positive")
+    
+}
