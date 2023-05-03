64c7735aaab294638f5587c61aa88d40971a691a
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-03 07:35:56 +0500

    chatgpt

---
+++ 29Converting strings to numbers, numbers to strings/app.js
@@ -0,0 +1,16 @@
+//In JavaScript, you can convert strings to numbers and vice versa using built-in functions. The two main functions are Number() and String().
+
+// //To convert a string to a number, you can use the Number() function. For example:
+// let numString="123";
+// let num=Number(numString);
+// console.log(num);
+// //In this example, the Number() function converts the string "123" to the number 123, which is stored in the num variable.
+
+//To convert a number to a string, you can use the String() function. For example:
+// let num=123;
+// let numString=String(num);
+// console.log(numString);
+let num = 123;
+let numString = String(num);
+
+console.log(numString); // Output: "123"
