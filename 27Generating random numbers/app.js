289cba8e986ea8bd70363c97060badc61a57aed2
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-03 06:54:35 +0500

    chatgpt

--- 27Generating random numbers/app.js
+++ 27Generating random numbers/app.js
@@ -1,5 +1,21 @@
 //In JavaScript, you can generate random numbers using the built-in Math.random() method. The Math.random() method returns a random decimal number between 0 (inclusive) and 1 (exclusive).
 
-//Here is an example of using the Math.random() method to generate a random number between 0 and 1:
-let num=Math.random();
-console.log("The random number is: " + num);
+// //Here is an example of using the Math.random() method to generate a random number between 0 and 1:
+// let num=Math.random();
+// console.log("The random number is: " + num);
+// //In this example, the Math.random() method is used to generate a random decimal number between 0 (inclusive) and 1 (exclusive). The random number is then stored in the variable num, and the output will be something like "The random number is: 0.8365212089277916".
+
+// //You can use the Math.floor() method in combination with Math.random() to generate a random integer between a minimum and maximum value. Here is an example:
+// let min =1;
+// let max =10;
+// let randomNumber=Math.floor(Math.random()*(max-min+1))+min;
+// console.log("The random number is: " + randomNumber);
+// //In this example, the Math.floor() method is used to round the result of Math.random() * (max - min + 1) down to the nearest integer. The max - min + 1 is used to ensure that the range includes the maximum value. The rounded number is then shifted by the min value to produce a random integer between min and max. The random number is then stored in the variable randomNum, and the output will be something like "The random number is: 7".
+
+// //Note that the Math.random() method generates a pseudo-random number, which means that the numbers are not truly random and can be predicted based on the algorithm used by the method. Therefore, the Math.random() method should not be used for security purposes or cryptography.
+
+
+
+
+
+
