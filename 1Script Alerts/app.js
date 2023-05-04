14e2ef2f661d100b7fb06809c811c323fad3338b
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-04 09:55:18 +0500

    SirishaqBhojani

---
+++ 1Script Alerts/app.js
@@ -0,0 +1,13 @@
+//In JavaScript, an alert is a built-in function that displays a message to the user in a pop-up dialog box. Here's how you can use the alert() function:
+
+// //alert("Hello, world!");
+// This will display a pop-up dialog box with the message "Hello, world!" and an OK button. When the user clicks the OK button, the dialog box will disappear.
+
+// You can also use variables and expressions in your alert messages by concatenating them with the + operator:
+
+// // let name = "Alice";
+// // let age = 30;
+// // alert("My name is " + name + " and I am " + age + " years old.");
+// This will display a pop-up dialog box with the message "My name is Alice and I am 30 years old." and an OK button.
+
+//Alerts are often used to display error messages or to provide feedback to the user. However, they can be disruptive to the user experience, so they should be used sparingly and only for important messages.
