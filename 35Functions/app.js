7b335346625dc58c73f95ebf31b8a65bc00a514d
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-05 17:08:53 +0500

    SirIshaqBhojani

---
+++ 35Functions/app.js
@@ -0,0 +1,11 @@
+//  var now = new Date();
+//  var theHr = now.getHours();
+//  var theMin = now.getMinutes();
+//  alert("time: " + theHr + ":" + theMin);
+
+function tellTime() {
+     var now = new Date();
+     var theHr = now.getHours();
+     var theMin = now.getMinutes();
+     alert("Current time: "+ theHr + ":" + theMin);
+    }
