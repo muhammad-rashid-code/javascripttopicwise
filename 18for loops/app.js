dc65428d8b3bacfef6418c1e30760acb778f76e0
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-10 09:13:31 +0500

    SirIshaqBhojani

---
+++ 18for loops/app.js
@@ -0,0 +1,27 @@
+//three type of loops
+//.1 for loop
+//.2 While loop 
+//.3 Do while loop
+
+// for (let  i= 1;  i<=10; i++) {
+//     console.log("2 " + "x "+i+" = "+i*2);
+    
+// }
+
+// let table=prompt("Enter A Number");
+// for (let  i= 1;  i<=10; i++) {
+//     console.log(table + "x "+i+" = "+i*table);
+//     }
+
+
+// let myName="Hello World"
+// for (var i=0; i<25; i++){
+//     console.log(myName);
+// }
+
+// const numbers=[1,2,3,4,5];
+// for(var i=0; i<numbers.length;i++){
+// console.log(numbers[i])
+// }
+
+    
