f8ace23b9281b3075d43ee3cf7d3e96a333277fa
MUHAMMAD RASHID <m.rashid212823@gmail.com>
2023-05-15 13:02:19 +0500

    calculator

--- JavaScriptMiscellaneous/app.js
+++ JavaScriptMiscellaneous/app.js
@@ -2,4 +2,13 @@
 // for( var i=0; i<10; i++){
 //     console.log("hello Karachi");
 // }
-// //console.log("hello Karachi");
+//console.log("hello Karachi");
+// for(i=1;i<=10;i++){
+// console.log("2 "+"x "+i+"= "+i*2)
+// }
+
+for(i=1;i<=10;i++){
+console.log("2 "+"x "+i+"= "+i*2)
+}
+
+
